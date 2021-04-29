5ff73036cfb653211b7d4c9b8942f7f28e8dfeff
Brian Haseley <bchaseley@gmail.com>
2021-04-29 12:48:07 -0700

    all pizzas done

---
+++ PizzaOven.js
@@ -0,0 +1,18 @@
+function pizzaOven(crustType, sauceType, cheese, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarela", "feta"],["mushrooms","olives","onions"] );
+console.log(pizza2)
+
+var pizza3 = pizzaOven("cauliflower crust", "white sauce", "no cheese", ["mushrooms", "spinach", "onions"]);
+console.log(pizza3)
+
